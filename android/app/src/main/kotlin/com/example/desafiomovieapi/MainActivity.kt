3fec836458ce9ae55b0a2c4bfdf87db9d289fe49
vfig29 <eliotefig@hotmail.com>
2021-04-28 06:15:39 -0300

    MVCEstrutura

---
+++ android/app/src/main/kotlin/com/example/desafiomovieapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafiomovieapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:desafiomovieapi/homeView.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MovieApp());
+}
+
+class MovieApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return HomeView();
+  }
+}
